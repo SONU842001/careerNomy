0390a5ad38c7faa060e7bef9b42ac8a49e3e2850
SONU842001 <54150646+SONU842001@users.noreply.github.com>
2022-05-01 16:07:37 +0530

    Added job card

--- resources/views/pages/job.blade.php
+++ resources/views/pages/job.blade.php
@@ -3,7 +3,7 @@
     <div class="container-fluid" style="padding-top: 48px; padding-left:16px; padding-right:16px;">
         <div class="container">
             <div class="row d-flex ">
-                <div class="col-lg-4">
+                <div class="col-lg-4 position-sticky">
                      <div class="card card1 px-4 shadow-sm mb-3" >
                          <div class="form-container">
                             <div class="row filter-ui">
@@ -75,6 +75,8 @@
                      </div>
                      </div>
                 </div>
+                {{-- content section --}}
+
                 <div class="col-lg-8">
                     <div class="card card2">
                        <div class="content-container">
@@ -96,20 +98,65 @@
                                 <i class="fas fa-home"></i>
                                 <span><a class="location_link" href="">Work From Home</a></span>
                                </div>
-                               <div class="internship_other_details">
-                                   <div class="other_detail_items_row">
-                                      <div class="other_detail_items">
+                               <div class="internship_other_details_containers">
+                                   <div class="other_detail_items_row d-flex">
+                                      <div class="other_detail_item">
                                           <div class="item_heading">
                                             <i class="fas fa-play-circle"></i>
-                                            <span>Start Date</span>
+                                            <span>START DATE</span>
+                                          </div>
+                                          <div class="item_body">
+                                              <span>Immediately</span>
+                                          </div>
+                                      </div>
+                                      <div class="other_detail_item">
+                                        <div class="item_heading">
+                                            <i class="fas fa-calendar-week"></i>
+                                          <span>Duration</span>
+                                        </div>
+                                        <div class="item_body">
+                                            <span>2 Weeks</span>
+                                        </div>
+                                    </div>
+                                    <div class="other_detail_items_row d-flex">
+                                        <div class="other_detail_item">
+                                            <div class="item_heading">
+                                                <i class="fas fa-money-bill"></i>
+                                              <span>STIPEND</span>
+                                            </div>
+                                            <div class="item_body">
+                                                <span>Unpaid</span>
+                                            </div>
+                                        </div>
+                                        <div class="other_detail_item">
+                                          <div class="item_heading">
+                                            <i class="fas fa-hourglass-end"></i>
+                                            <span>Apply By</span>
+                                          </div>
+                                          <div class="item_body">
+                                              <span>2 May' 22</span>
                                           </div>
                                       </div>
                                    </div>
                                </div>
                            </div>
+                           <div class="button_container d-flex justify-content-space-between">
+                               <div class="tag_container d-flex">
+                                   <div class="label_container">
+                                       Internship
+                                   </div>
+
+                               </div>
+                               <a href="" class="view_details">
+                                <div class="view_details_div">View details</div>
+                                <i class="fas fa-angle-right"></i>
+                            </a>
+                           </div>
                        </div>
                     </div>
                 </div>
+
+
 
             </div>
 
